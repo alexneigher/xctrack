6fd45a8a6f5c59209bb28cc867672bad07f3aa8f
Alex Neigher <alex.neigher@snapdocs.com>
2016-08-19 08:49:12 -0700

    devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 
+  validates :email, presence: true, uniqueness: true
+  validates :name, :share_url, presence: true
 
   def full_api_url
     d1 = formatted_datetime(1.day.ago)
