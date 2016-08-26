22cffb0df931c3c8c5419e8d64890d1eef27f661
Alex Neigher <alex.neigher@snapdocs.com>
2016-08-25 22:49:23 -0700

    fixing security

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,15 @@ class ApplicationController < ActionController::Base
 
   private
 
-    def fetch_users(group = nil)
+    #make sure only admin users have access to certain pages
+    def authenticate_admin_access
+      unless current_user.admin? || @user == current_user
+        flash[:error] = 'Bad Robot'
+        redirect_to users_path
+      end
+    end
 
+    def fetch_users(group = nil)
       if group
         @users = group.users
       else
@@ -19,7 +26,6 @@ class ApplicationController < ActionController::Base
       else
         @users_on_map = []
       end
-
       @rendered_user_ids = @users_on_map.map(&:id)
     end
 end
