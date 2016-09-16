71cf029c029d9b1871c79786a6b7fdca62fbadb5
Alex Neigher <alex.neigher@snapdocs.com>
2016-09-16 15:33:16 -0700

    better title helper for facebook

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,20 @@
 module ApplicationHelper
 
+  def title_helper
+    base_string = 'XcTrack - The Mobile Pilot Tracker'
+    return base_string unless @group || @users_on_map
+
+    if @group
+      str = "- #{@group.name}"
+    elsif @users_on_map.length > 1
+      str = 'Multiple Active Pilots'
+    elsif @users_on_map.length == 1
+      str = "- #{@users_on_map.first.name}"
+    end
+
+    return "#{base_string} #{str}"
+  end
+
   def add_user_to_map(rendered_user_ids, user, current_group, *args, &block)
     if current_group
       link_to group_path(current_group, pilots: (rendered_user_ids + [user.id]).join(',')), *args, &block
