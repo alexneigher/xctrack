e86c178c7ef8b743985b7fa67081ead7741790db
Alex Neigher <aneigher@gmail.com>
2017-06-17 22:33:30 -0600

    order by distance

--- app/assets/javascripts/ui.js
+++ app/assets/javascripts/ui.js
@@ -1,16 +1,5 @@
 $(function(){
 
-  $('.order-by-name').click(function(){
-      var divList = $(".pilot-on-map");
-      divList.sort(function(a, b){
-
-        return $(a).data("name") > $(b).data("name")
-      });
-
-      $("#pilot-names").html(divList);
-
-  })
-
   $('.order-by-distance').click(function(){
     var divList = $('.pilot-on-map');
     divList.sort(function(a, b){
@@ -21,6 +10,9 @@ $(function(){
     });
 
     $("#pilot-names").html(divList);
+
+    $(this).parents('.list-group-item').remove();
+
   })
 
 
