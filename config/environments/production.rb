1bd11bc4d63fe0af9235226c574972b50b34fc96
Alex Neigher <aneigher@gmail.com>
2017-10-16 17:19:04 -0600

    fixing iframe action

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,10 @@ Rails.application.configure do
   # NGINX, varnish or squid.
   # config.action_dispatch.rack_cache = true
 
+  config.action_dispatch.default_headers = {
+      'X-Frame-Options' => 'ALLOWALL'
+  }
+  
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
