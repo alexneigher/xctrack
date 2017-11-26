81acefffc78ca78d97b2e484b86876916e91e20d
Alex Neigher <aneigher@gmail.com>
2017-11-26 14:22:20 -0800

    force ssl

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  force_ssl if: :ssl_configured?
 
   private
 
@@ -13,5 +14,9 @@ class ApplicationController < ActionController::Base
       @users_on_map = user_collector.users_on_map
 
       @rendered_user_ids = user_collector.rendered_user_ids
+    end
+
+    def ssl_configured?
+      !Rails.env.development?
     end
 end
