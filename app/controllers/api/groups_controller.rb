373abd0ff8c5682f7d10d050482d57d3bf723140
Alex Neigher <alexneigher@Alexs-MacBook-Pro.local>
2020-08-27 12:55:53 -0700

    remove api

--- app/controllers/api/groups_controller.rb
+++ app/controllers/api/groups_controller.rb
@@ -19,27 +19,5 @@ module Api
       render json: {last_known_waypoints: @most_recent_waypoints}
     end
 
-    def all_sar_trackpoints
-      @most_recent_waypoints = []
-      @group = Group.find(params[:group_id])
-      users = @group.users
-
-      users.all.each do |user|
-        user.most_recent_flight&.destroy
-        user.reload
-        hour = (DateTime.now.to_time - DateTime.parse('22nd Aug 2020 10:05:06+07:00').to_time) / 1.hours
-        response = HTTParty.get(user.full_api_url(hour)).body
-        raw_xml = Nokogiri::XML(response)
-
-        CoordinateFetcherService.new(raw_xml, user).extract_coordinates
-      end
-
-      users.reload.each do |user|
-        @most_recent_waypoints << user.waypoints.order(:created_at)
-      end
-
-      render json: {data: @most_recent_waypoints.flatten}
-    end
-
   end
 end
